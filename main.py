4d5d87a1265893ce3929e54bbf622bede95bd92e
Kaiser Pister <pisterk@gmail.com>
2022-12-04 15:41:28 -0600

    add grammar correction

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ import cohere
 from dotenv import load_dotenv
 
 
+load_dotenv()
 # website: https://sincereley-darcy.unicornplatform.page/
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keys.json"
-config = load_dotenv()
+cohere_api_key = os.getenv("cohere_api_key")
+password = os.getenv("password")
+co = cohere.Client(cohere_api_key)
 
 # use google document AI to extract text from png
 def extract_text_from_png(file_path):
@@ -23,20 +26,43 @@ def extract_text_from_png(file_path):
     return document.text
 
 
-# generate response with cohere
-def generate_response(text):
-    preprompt = ""
-    co = cohere.Client(config["cohere_api_key"])
+def fix_grammar(text):
+    end = text.split("\n")[-1]
+    text = text.replace("\n", " ")
+    prompt = (
+        f"Please fix the grammar in the following letter.\n\nOriginal: {text}\n\nFixed:"
+    )
+    print(prompt)
     response = co.generate(
         model="xlarge",
-        prompt=preprompt + text,
-        max_tokens=300,
+        prompt=prompt,
+        max_tokens=200,
+        temperature=0.4,
+        k=0,
+        p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop_sequences=[end],
+        return_likelihoods="NONE",
+    )
+    return response.generations[0].text
+
+
+# generate response with cohere
+def generate_response(text):
+    preprompt = "Write a short letter in the style of an 18th century letter. Use at most 500 characters. Become the character Mr. Darcy from Pride and Prejudice. Please answer all the questions in the letter, and be sure to ask at least one question.\nLetter: "
+    postprompt = "\n\nDarcy's Response:"
+    prompt = preprompt + text + postprompt
+    response = co.generate(
+        model="xlarge",
+        prompt=prompt,
+        max_tokens=100,
         temperature=0.8,
         k=0,
         p=1,
         frequency_penalty=0,
         presence_penalty=0,
-        stop_sequences=["Darcy."],
+        stop_sequences=["Darcy"],
         return_likelihoods="NONE",
     )
 
@@ -72,11 +98,11 @@ def send_card(message, address):
     recipient_city = urllib.parse.quote(address["city"])
     recipient_state = urllib.parse.quote(address["state"])
     recipient_zip = urllib.parse.quote(address["zip"])
-    password = config["password"]
 
     data = f"login={login}&password={password}&message={message}&card_id={card_id}&font_label={font_label}&credit_card_id={credit_card_id}&sender_name={sender_name}&sender_address1={sender_address1}&sender_address2={sender_address2}&sender_city={sender_city}&sender_state={sender_state}&sender_zip={sender_zip}&recipient_name={recipient_name}&recipient_address1={recipient_address1}&recipient_address2={recipient_address2}&recipient_city={recipient_city}&recipient_state={recipient_state}&recipient_zip={recipient_zip}"
 
-    return requests.post(order_url, data=data, headers=headers)
+    res = requests.post(order_url, data=data, headers=headers)
+    return res
 
 
 def load_address():
@@ -92,12 +118,17 @@ def load_address():
 
 
 def main():
-    file_name = os.path.abspath("img.jpg")
+    file_name = os.path.abspath("letter.jpg")
     note = extract_text_from_png(file_name)
+    note = fix_grammar(note)
+    print(note)
     message = generate_response(note)
+    if len(message) > 500:
+        message = message[:500]
+    print(message)
     address = load_address()
-    handwrytten_response = send_card(message, address)
-    if handwrytten_response.status_code == 200:
-        print("success")
-    else:
-        print("failure")
+    send_card(message, address)
+
+
+if __name__ == "__main__":
+    main()
